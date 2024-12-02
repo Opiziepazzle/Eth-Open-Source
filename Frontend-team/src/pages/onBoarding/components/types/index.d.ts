f203298bb20026a592a39db5b0bd2fb6864e924a
soyaya <davidlovedavid1015@gmail.com>
2024-12-02 13:05:47 +0100

    Reset remote main to empty state

--- Frontend-team/src/pages/onBoarding/components/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export interface FirstScreenProps {
-    firstIndex: number; // The current index for navigation
-    setFirstIndex: Dispatch<SetStateAction<number>>; // Function to update the index
-  }
